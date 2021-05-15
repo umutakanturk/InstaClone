2882a6d3bd34452afac95d75aa53ab500a86656d
umutakanturk <umutaknturk@gmail.com>
2021-05-15 16:44:51 +0300

    Project created
    login operations and firebase integration have been done.

---
+++ app/src/main/java/com/example/instaclone/UploadActivity.java
@@ -0,0 +1,21 @@
+package com.example.instaclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class UploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
+    public void upload(View view){
+
+    }
+    public void selectImage(View view){
+
+    }
+}
